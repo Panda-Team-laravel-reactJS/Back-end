02ee3ce0f0702ddde95e6882c632b42f9ce15510
{Tên}TamDinh <{tamdinh793@gmail.com}>
2022-07-16 19:19:42 +0700

    API Account

---
+++ app/Http/Controllers/api/ApiCustomerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controller\Api;
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+class ApiCustomerController extends Controller{
+    public function displayCustomer(){
+        
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ApiAccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::prefix("/account/")->group(function(){
+    Route::post("register",[ApiAccountController::class,"register"]);
+    Route::post("login",[ApiAccountController::class,"login"]);
 });
