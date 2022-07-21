ebd421a7b83e3e955ae45755132737aa03570494
NDK2509 <Dangkyqt2509@gmail.com>
2022-07-22 01:51:54 +0700

    Update AdminAuth middleware

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Admin;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 
 class AdminAuth
 {
@@ -16,6 +19,10 @@ class AdminAuth
      */
     public function handle(Request $request, Closure $next)
     {
+        $adminSession = Session(ADMIN);
+        if (empty($adminSession) || Admin::find($adminSession->userName)->access_token != $adminSession->accessToken) {
+            return redirect()->route("login");
+        }  
         return $next($request);
     }
 }
