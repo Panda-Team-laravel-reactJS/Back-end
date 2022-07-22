a1153e11c9b6be00ca0bc210523f1b48715d9608
NDK2509 <Dangkyqt2509@gmail.com>
2022-07-22 11:39:16 +0700

    Update config/app.php

--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        "AdminLogin" => App\Core\AdminLogin\AdminLogin\AdminLogin::class,
     ])->toArray(),
 
 ];
