5500077118687a9ae56f34d54d5eed7eb316ffe2
NDK2509 <Dangkyqt2509@gmail.com>
2022-07-24 22:04:29 +0700

    Update AdminLogin: Logout function, generate acess_token

--- app/Core/AdminLogin/AdminLogin.php
+++ app/Core/AdminLogin/AdminLogin.php
@@ -1,6 +1,7 @@
 <?php 
 namespace App\Core\AdminLogin;
 
+use App\Core\Constants\SessionConstants;
 use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -11,15 +12,20 @@ class AdminLogin {
         $admin = Admin::find($request->username);
         $check = Hash::check($request->password, $admin->password);
         if ($check) {
-            Session::put(ADMIN, (new AdminSession($admin))->toArray());
+            Session::put(SessionConstants::ADMIN, (new AdminSession($admin))->toArray());
         }
+        return Session(SessionConstants::ADMIN);
         return $check;
     }
+    public static function logout() {
+        Session::forget(SessionConstants::ADMIN);
+        return redirect()->route("login");
+    }
     public static function check() {
-        return !empty(Session(ADMIN));
+        return !empty(Session(SessionConstants::ADMIN));
     }
     public static function admin() {
-        return Session(ADMIN);
+        return Session(SessionConstants::ADMIN);
     }
     
 }
--- app/Core/AdminLogin/AdminSession.php
+++ app/Core/AdminLogin/AdminSession.php
@@ -2,16 +2,21 @@
 namespace App\Core\AdminLogin;
 
 use App\Models\Admin;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class AdminSession {
     private string $userName;
     private string $accessToken;
     public function __construct(Admin $admin) {
         $this->userName = $admin->username;
-        $this->accessToken = $admin->access_token;
+        $this->generateAccessToken($admin);
     }
     public function toArray() {
         return [ "userName" => $this->userName, "accessToken" => $this->accessToken ];
+    }
+    private function generateAccessToken(Admin $admin) {
+        $admin->access_token = Str::random(30);
+        $this->accessToken = Hash::make($admin->access_token);
     }
 }
 ?>
